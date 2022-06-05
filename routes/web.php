70574cbf1b2ef980645aaa47026cab14519f00ea
Ricardo <jo316.dev@gmail.com>
2022-06-05 17:16:20 -0300

    Queue OK

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Events\PostCreated;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('post', function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    $user = User::first();
+
+    $post = $user->posts()->create([
+        'title' => 'Hoje é domingo frio',
+        'body' => 'O dia hoje ta muito frio ta doido'
+    ]);
+
+    return 'ok';
+
+    // event(new PostCreated($post));
 });
